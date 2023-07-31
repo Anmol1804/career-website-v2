034a6359f47eb193474017a9f3e0c9602c7f4952
Replit user <>
2023-07-31 17:17:27 +0000

    load from db display

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 # connecting database
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 
 db_conn_string = "mysql+pymysql://7wx4q7vlaxmzkf5c0jpb:pscale_pw_2VltpEPEiFnU9A9J0ZLF76hrkwQYeQNlUwtinP27Kxk@aws.connect.psdb.cloud/careers?charset=utf8mb4"
 
@@ -15,18 +15,7 @@ engine = create_engine(db_conn_string, connect_args={
 # result.all() -> list type
 # result.all()[0] -> row
 
-# function to map values
-keys = ['id','title','location','salary','currency','responsibilities','requirements']
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))  
-
-  result_dict = []
-  for row in result.all():
-    row_val_ls = list(row)
-    result_dict.append(dict(zip(keys,row_val_ls)))
-
-  print(result_dict)
   
   # result_all = result.all()
   # print(result_all)
--- main.py
+++ main.py
@@ -1,38 +1,30 @@
 from flask import Flask, render_template, jsonify
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': '1',
-    'title': 'Data Analyst',
-    'location': 'Bengalaru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': '2',
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': '3',
-    'title': 'Frontend engineer',
-    'location': 'remote',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': '4',
-    'title': 'Backend Engineer',
-    'location': 'Pune, India',
-    # 'salary': '$120,000'
-  }
-]
 
+def load_jobs_from_db():
+  
+  # list to map values
+  keys = ['id','title','location','salary','currency','responsibilities','requirements']
+  
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))  
+  
+    jobs = []
+    for row in result.all():
+      row_val_ls = list(row)
+      jobs.append(dict(zip(keys,row_val_ls)))
+  
+    return jobs
 
 @app.route("/")
 def helloWorld():
+  JOBS = load_jobs_from_db()
   return render_template('home.html', jobs=JOBS)
+
 
 
 # we store the data in an api route (json data)
